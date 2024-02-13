af493f09f32e033d06493c521e52362c844a1f3a
Nima S. Azar <nima.azar92@gmail.com>
2024-02-13 17:04:55 +1100

    Data validation added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from MLProjectMLflow import logger
 from MLProjectMLflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from MLProjectMLflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLProjectMLflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -20,15 +20,15 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Data Validation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataValidationTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
