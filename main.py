231919728b8192ddaa2a9974d362434aea3e703b
Nima S. Azar <nima.azar92@gmail.com>
2024-02-14 08:53:52 +1100

    Data transformation added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from MLProjectMLflow import logger
 from MLProjectMLflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLProjectMLflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from MLProjectMLflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLProjectMLflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
@@ -32,15 +32,15 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Data Transformation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataTransformationTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
