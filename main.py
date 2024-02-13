b95aa387369ea82acb70515343cd8cdda2689fe4
Nima S. Azar <nima.azar92@gmail.com>
2024-02-13 15:58:10 +1100

    data ingestion added

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from MLProjectMLflow import logger
-# from MLProjectMLflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from MLProjectMLflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from MLProjectMLflow.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
-logger.info("Welcome to Nima's Project")
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+# logger.info("Welcome to Nima's Project")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
