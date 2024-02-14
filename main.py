e509aa681132ee7a0f45f0859282d384fa374d05
Nima S. Azar <nima.azar92@gmail.com>
2024-02-14 11:52:55 +1100

    Model evaluation and mlflow added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from MLProjectMLflow.pipeline.stage_01_data_ingestion import DataIngestionTraini
 from MLProjectMLflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLProjectMLflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from MLProjectMLflow.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-# from MLProjectMLflow.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+from MLProjectMLflow.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 # logger.info("Welcome to Nima's Project")
 
@@ -57,15 +57,15 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Model evaluation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = ModelEvaluationTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
